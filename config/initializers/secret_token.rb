795cb5dcedf409505ac31db8e11cd7633f860e2c
U-TinyMonolith\Trent <Trent@TinyMonolith.(none)>
2011-03-12 17:06:03 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a545064324a3b71a67181b84349abd776412fb49ed2e5eb19acc0ad7a8e810a086fb46dc1d9f58537b6aaee49cb7a6c4c6798a161cf1af6a651f26b5786ac2bf'
